40756ef0063665ea8fe409c5c1ed45b6dcd34e1a
yshong1998 <yshong1998@gmail.com>
2025-12-08 13:40:10 +0900

    initial commit

---
+++ src/main/java/com/ssafy/bablog/BaBLogBeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.bablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaBLogBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaBLogBeApplication.class, args);
+    }
+
+}
